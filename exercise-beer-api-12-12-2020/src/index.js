519ad4ca3c28e56200e1254227d31927b9085131
David Aguilar Alfayate <davidaguilaralfayate@MacBook-Pro-de-David.local>
2021-01-16 15:13:04 +0100

    beer

--- exercise-beer-api-12-12-2020/src/index.js
+++ exercise-beer-api-12-12-2020/src/index.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const { SERVER_PORT } = require('./constants')
+const beersRoute = require('./routes/beers')
+
 // We need require proper beers routes module
 // ...
 
+const path = require('path')
+
 const app = express()
+app.use('/beers', beersRoute)
+
+
+
 
 app.use(bodyParser.json())
 
--- exercise-beer-api-12-12-2020/src/routes/beers.js
+++ exercise-beer-api-12-12-2020/src/routes/beers.js
@@ -1,6 +1,93 @@
 const express = require('express')
 const router = express.Router()
+const fs = require('fs')
+const { defaultMaxListeners } = require('stream')
 
 // All your beers services go here...
+
+const BEERS_N = 'src/db/beers.json'
+
+router.get('/random', (req, res) => {
+    fs.readFile(BEERS_N, function (err, data) {    
+        if (!err) {
+            const dataToJson = JSON.parse(data)
+            const randomNumber = Math.random()*300;
+            const randomNumberR = Math.round(randomNumber)
+
+            const dataBeersRandom = dataToJson.filter(function (beer) {
+                    
+                return beer.id === randomNumberR                  
+            })
+
+            
+            
+            
+            res.status(200).json({
+                success: true,
+                data: dataBeersRandom,
+                
+            })
+        }
+          else {
+            res.status(500).json({
+                success: false,
+                message: "muy mal"
+            })
+        }
+    })
+}) 
+
+router.get('/', (req, res) => {
+    fs.readFile(BEERS_N, function (err, data) {    
+        if (!err) {
+            const dataToJson = JSON.parse(data)
+            const data25 = dataToJson.slice(0,25);
+            res.status(200).json({
+                success: true,
+                data: data25,
+                length: data25.length
+            })
+        }
+          else {
+            res.status(500).json({
+                success: false,
+                message: "muy mal"
+            })
+        }
+    })
+})
+
+router.get('/:id', (req, res) => {
+     fs.readFile(BEERS_N, function (err, data) {
+            if (!err) {
+                const dataIdBeers =  JSON.parse(data)
+                const dataBeers = dataIdBeers.filter(function (beer) {
+                    
+                    return beer.id === Number(req.params.id)                  
+                })
+                res.status(200).json({
+                    success: true,
+                    data: dataBeers,
+                })
+
+            }       
+
+            else {
+                res.status(500).json({
+                    success: false,
+                    message: "hay que meter el dato"
+                })
+            }
+
+        }
+
+
+    
+    
+    )
+
+})
+ 
+
 
 module.exports = router
